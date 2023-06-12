d16881fd925463b231b6005c2a272148bcd6329a
Sol Laudon <sollaudon@Sols-MacBook-Pro.local>
2023-06-12 12:13:18 -0500

    trying to add html through js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const app = express();  
 
 app.listen(3000, () => {
   console.log("Application started and Listening on port 3000");
---
+++ forms.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let myName = 'Sol'; console.log('hello');
+    let m = 'Sol'; console.log('hello'); let o = 0;
+    
+    // const $hello = document.getElementsByClassName('hello');
+    // console.log($hello);
+  }, false);
+
+
