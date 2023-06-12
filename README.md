6be03020c620b45c4794a7c23bb5320798a528a8
Sol Laudon <sollaudon@Sols-MacBook-Pro.local>
2023-06-12 08:36:53 -0500

    replace all files and follow section.io setup

---
+++ README.md
@@ -0,0 +1,2 @@
+Install node, express, and nodemon
+Run 'nodemon' to start the server and navigate to localhost:3000 in browser to verify server is working
--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-$( document ).ready(function() {
-    console.log('hello');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", (req, res) => {
+    var subName = req.body.yourname
+    var subEmail = req.body.youremail;
+   res.send("Hello " + subName + ", Thank you for subcribing. You email is " + subEmail);
 });
