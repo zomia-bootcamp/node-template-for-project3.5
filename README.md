9d3f851a97ecf0a84dfe52332cb3562b2b36e854
Sol Laudon <sollaudon@Sols-MacBook-Pro.local>
2023-06-13 08:28:19 -0500

    clean up app.js

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Install node, express, and nodemon
+Install node, express, nodemon, jsdom, and jquery
 
 Run 'nodemon' to start the server and navigate to localhost:3000 in browser to verify server is working
 
-Using body-parser to get form data
+Using body-parser to get form data for POST
--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();  
-
-var jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-var $ = jQuery = require('jquery')(window);
+const app = express();
 
 app.listen(3000, () => {
   console.log("Application started and Listening on port 3000");
